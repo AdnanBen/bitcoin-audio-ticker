6dd9eb1d5cb06c8d8ad729ec24ebef1660e3c94f
adnanben <adnanb1112@gmail.com>
2022-10-14 08:18:40 +0100

    Add basic text ticker Add demo sound on price change

--- src/App.tsx
+++ src/App.tsx
@@ -1,23 +1,43 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { useEffect, useState } from "react";
+import logo from "./logo.svg";
+import "./App.css";
 
 function App() {
+  const [start, setStart] = useState(false);
+  const [btcPrice, setBtcPrice] = React.useState("");
+  var audioUrl = "https://freewavesamples.com/files/Ensoniq-ESQ-1-Piano-C3.wav";
+
+  useEffect(() => {
+    console.log("Page Load");
+  }, []);
+
+  useEffect(() => {
+    if (start) {
+      const interval = setInterval(() => {
+        fetch("https://api.coinbase.com/v2/prices/BTC-USD/buy")
+          .then((res) => res.json())
+          .then((data) => {
+            console.log("called");
+            setBtcPrice(data.data.amount);
+          });
+      }, 1000);
+      return () => clearInterval(interval);
+    }
+  }, [start]);
+
+  useEffect(() => {
+    console.log(btcPrice);
+    new Audio(audioUrl).play();
+  }, [btcPrice, setBtcPrice]);
+
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        {start ? (
+          <p>Price is now ${btcPrice} USD</p>
+        ) : (
+          <p onClick={() => setStart(true)}>Click here to start</p>
+        )}
       </header>
     </div>
   );
