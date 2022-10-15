426ffeb5151d1cd8767fcd48a8cb6303dd9f78d2
adnanben <adnanb1112@gmail.com>
2022-10-15 02:20:54 +0100

    Add real time graph

--- src/App.tsx
+++ src/App.tsx
@@ -1,21 +1,74 @@
-import React, { useEffect, useState } from "react";
+import React, { useEffect, useState, useRef } from "react";
 import logo from "./logo.svg";
 import "./App.css";
 import useWebSocket, { ReadyState } from "react-use-websocket";
-import { JsonValue } from "react-use-websocket/dist/lib/types";
+import Chart from "react-apexcharts";
+import ReactApexChart from "react-apexcharts";
 
 function App() {
   const [start, setStart] = useState(false);
-  const [btcPrice, setBtcPrice] = React.useState("");
+  const [btcPrice, setBtcPrice] = React.useState(19120);
   var audioUrl = "https://freewavesamples.com/files/Ensoniq-ESQ-1-Piano-C3.wav";
+
+  const [data, setData] = useState([19120, 19125]);
+
+  const [state, setState] = useState({
+    series: [
+      {
+        data: [19080, 19080, 19082],
+      },
+    ],
+    options: {
+      chart: {
+        height: 350,
+        type: "line" as ApexChart["type"],
+        animations: {
+          enabled: true,
+          easing: "linear" as Exclude<
+            ApexChart["animations"],
+            undefined
+          >["easing"],
+          dynamicAnimation: {
+            speed: 1000,
+          },
+        },
+        zoom: {
+          enabled: false,
+        },
+        toolbar: {
+          show: false,
+        },
+      },
+      dataLabels: {
+        enabled: false,
+      },
+      stroke: {
+        curve: "smooth" as ApexStroke["curve"],
+      },
+      markers: {
+        size: 0,
+      },
+      grid: {
+        row: {
+          colors: ["#f3f3f3", "transparent"], // takes an array which will be repeated on columns
+          opacity: 0.5,
+        },
+      },
+      yaxis: {
+        max: 19250,
+        min: 19190,
+      },
+      xaxis: { range: 10 },
+    },
+  });
 
   const { readyState, sendMessage, lastJsonMessage } = useWebSocket(
     "wss://ws-feed.exchange.coinbase.com"
   );
 
   useEffect(() => {
-    console.log("Page Load");
-  }, []);
+    console.log("rerender");
+  });
 
   useEffect(() => {
     const jsonBody = {
@@ -31,18 +84,47 @@ function App() {
   }, [readyState]);
 
   useEffect(() => {
-    console.log((lastJsonMessage as any)?.price);
     setBtcPrice((lastJsonMessage as any)?.price);
   }, [lastJsonMessage]);
 
   useEffect(() => {
-    console.log(btcPrice);
-    new Audio(audioUrl).play();
+    // new Audio(audioUrl).play();
   }, [btcPrice, setBtcPrice]);
 
+  useEffect(() => {
+    const timeout = setInterval(() => {
+      console.log("new price is " + btcPrice);
+
+      console.log(data.length);
+      // setState((prev) => {
+      //   let tempVar = JSON.parse(JSON.stringify(prev));
+      //   tempVar.series[0]["data"].push(btcPrice);
+      //   console.log(tempVar);
+      //   return tempVar;
+      // });
+
+      setState((prev) => {
+        const deepArrClone = JSON.parse(JSON.stringify(prev.series));
+        deepArrClone[0]["data"].push(btcPrice);
+        return { ...prev, series: deepArrClone };
+      });
+    }, 1000);
+
+    console.log(state);
+
+    return () => clearInterval(timeout);
+  }, [state]);
+
   return (
+    // <div>
+    //   <header>
     <div className="App">
-      <header className="App-header">
+      <header>
+        <ReactApexChart
+          options={state.options}
+          series={state.series}
+          height={1000}
+        />
         {start ? (
           <p>Price is now ${btcPrice} USD</p>
         ) : (
